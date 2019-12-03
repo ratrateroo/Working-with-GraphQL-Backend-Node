99062b571bf905874017e13dba449132158eeeb2
Mark Leo <ratrateroo@gmail.com>
2019-12-04 04:00:47 +0800

    extracted user data from the auth token

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const graphqlHttp = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const { MongoDBUsername, MongoDBPassword } = require('./config/config');
 const DATABASE_NAME = 'messages';
@@ -56,6 +57,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth);
 
 app.use(
   '/graphql',
